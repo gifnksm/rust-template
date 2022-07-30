62ba7aceff292426f38d99f2c1101193b0d6f8fb
gifnksm <makoto.nksm+github@gmail.com>
2022-07-31 01:27:12 +0900

    First commit

---
+++ template/src/lib.rs
@@ -0,0 +1,24 @@
+{%- if crate_type == "lib" -%}
+//! {{ project-description }}
+//!
+//! # Usage
+//!
+//! Add this to your `Cargo.toml`:
+//!
+//! ```toml
+//! [dependencies]
+//! {{ project-name }} = "0.1.0"
+//! ```
+
+#![doc(html_root_url = "https://docs.rs/{{project-name}}/0.1.0")]
+
+{% endif -%}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
